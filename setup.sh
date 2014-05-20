5ea97547c699fcda331657d176405912bf69179b
amedw <aedwards@thoughtworks.com>
2014-05-20 22:13:26 +1000

    Fixed the setup script and added output

--- setup.sh
+++ setup.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
 
-if [ -x $HOME/.vimrc ]; then mv $HOME/.vimrc{,_bak}; fi
-if [ -x $HOME/.vim ]; then mv $HOME/.vim{,_bak}; fi
+if [ -L $HOME/.vimrc ]
+  then 
+    echo "removing symbolic link ~/.vimrc"
+    rm $HOME/.vimrc
+fi
 
+if [ -L $HOME/.vim ]
+  then 
+    echo "removing symbolic link ~/.vim"
+    rm $HOME/.vim
+fi
+
+if [ -x $HOME/.vimrc ] 
+  then 
+    echo "moving ~/.vimrc to ~/.vimrc_bak"
+    mv $HOME/.vimrc{,_bak} 
+fi
+
+if [ -d $HOME/.vim ]
+  then 
+    echo "moving folder ~/.vim to ~/.vim_bak"
+    mv $HOME/.vim{,_bak}
+fi
+
+echo "linking new .vimrc"
 ln -s `pwd`/.vimrc ~/.vimrc
+
+echo "linking new .vim"
 ln -s `pwd`/.vim ~/.vim
